466c7b97edda7c95be18e39fcd5be353aec45685
Sana <sanaq1992@live.com>
2023-07-02 21:45:08 -0500

    JS temp function

---
+++ index.js
@@ -0,0 +1,10 @@
+// celcius to f
+// f = c * 1.8 + 32
+
+function convertCelsiusToFarenheit(celsius) {
+    return celsius * 1.8 + 32
+}
+
+console.log(convertCelsiusToFarenheit(10))
+console.log(convertCelsiusToFarenheit(100))
+console.log(convertCelsiusToFarenheit(10))
