9801243bcefb5d68b192b269f19c9e99c3c681df
Sana <sanaq1992@live.com>
2023-07-05 21:21:38 -0500

    practice JS drills

--- index.js
+++ index.js
@@ -6,30 +6,97 @@
 
 // console.log(lessThanOrEqualToZero(- 1)); 
 
-function animalSound(animal) {
-    if (animal === "cat") {
-      return "Meow!";
-    } else if (animal === "dog") {
-      return "Woof!";
-    } else {
-      return "Unknown animal!";
-    }
-  }
+// function animalSound(animal) {
+//     if (animal === "cat") {
+//       return "Meow!";
+//     } else if (animal === "dog") {
+//       return "Woof!";
+//     } else {
+//       return "Unknown animal!";
+//     }
+//   }
 
-  console.log(animalSound("dog"))
+//   console.log(animalSound("dog"))
 
-  function sum(a, b) {
-    return a + b;
-  }
+//   function sum(a, b) {
+//     return a + b;
+//   }
 
-  console.log(sum(4, 5));
+//   console.log(sum(4, 5));
 
-  function evenOrOdd(number) {
-    if (number % 2 === 0) {
-      return "Even";
-    } else {
-      return "Odd";
-    }
-  }
+//   function evenOrOdd(number) {
+//     if (number % 2 === 0) {
+//       return "Even";
+//     } else {
+//       return "Odd";
+//     }
+//   }
 
-  console.log(evenOrOdd(34));
+//   console.log(evenOrOdd(34));
+
+// printNumbers(10)
+
+// function printNumbers(n) {
+//     for (let i = 1; i <= n; i++) {
+//       console.log(i);
+//     }
+//   }
+
+
+//   function printEvenNumbers(num) {
+//     for (let num = 1; num <= 100; num++) {
+//         if (num % 2 === 0) {
+//         console.log(num)
+//     }
+//   }
+// }
+
+//   printEvenNumbers(10)
+
+// Exercise 3: Calculate Sum
+// Write a function called calculateSum that takes a number 
+// as a parameter and calculates 
+// the sum of all the numbers from 1 to that number. 
+// Return the sum.
+// calcSum(10)
+
+// function calcSum(num) {
+//     for (let num = 1; num <= 20; num++) {
+//    console.log(calcSum);
+//     }
+// }
+
+
+// let count = 1
+
+// while (count <= 100) {
+// console.log(count)
+// count = count + 1
+// }
+
+// for (let i = 1; i <= 20; i++){
+//    if (i % 3 === 0 && i % 5 === 0) {
+//         console.log(`${i} --> FrontendSimplified`)
+//     }
+//     else if (i % 3 === 0) {
+//         console.log(`${i} --> frontend`)
+//     }
+//     else if (i % 5 === 0) {
+//         console.log(`${i} --> simplified`)
+//     }
+    
+//     else (console.log(`${i} --> ${i}`))
+// }
+
+// let str = "Frontend Simplified"
+
+// for (i = 0; i <= str.length; i++) {
+//     console.log(str[i])
+// }
+
+function welcomeNameToFes(name, age) {
+    console.log(`Welcome ${name} ${25} to FES`)
+    // console.log(name)
+}
+
+welcomeNameToFes('David', '25');
