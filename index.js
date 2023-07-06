39d7589ef88fa0171ac85e23ec0e6bc9602d49d9
Sana <sanaq1992@live.com>
2023-07-05 21:27:50 -0500

    adding more drills

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-// let num1 = 115;
+let num1 = 115;
 
-// function lessThanOrEqualToZero(num1) {
-//     return num1 <= 0;
-// }
+function lessThanOrEqualToZero(num1) {
+    return num1 <= 0;
+}
 
-// console.log(lessThanOrEqualToZero(- 1)); 
+console.log(lessThanOrEqualToZero(- 1)); 
 
-// function animalSound(animal) {
-//     if (animal === "cat") {
-//       return "Meow!";
-//     } else if (animal === "dog") {
-//       return "Woof!";
-//     } else {
-//       return "Unknown animal!";
-//     }
-//   }
+function animalSound(animal) {
+    if (animal === "cat") {
+      return "Meow!";
+    } else if (animal === "dog") {
+      return "Woof!";
+    } else {
+      return "Unknown animal!";
+    }
+  }
 
-//   console.log(animalSound("dog"))
+  console.log(animalSound("dog"))
 
-//   function sum(a, b) {
-//     return a + b;
-//   }
+  function sum(a, b) {
+    return a + b;
+  }
 
-//   console.log(sum(4, 5));
+  console.log(sum(4, 5));
 
-//   function evenOrOdd(number) {
-//     if (number % 2 === 0) {
-//       return "Even";
-//     } else {
-//       return "Odd";
-//     }
-//   }
+  function evenOrOdd(number) {
+    if (number % 2 === 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+  }
 
-//   console.log(evenOrOdd(34));
+  console.log(evenOrOdd(34));
 
-// printNumbers(10)
+printNumbers(10)
 
-// function printNumbers(n) {
-//     for (let i = 1; i <= n; i++) {
-//       console.log(i);
-//     }
-//   }
+function printNumbers(n) {
+    for (let i = 1; i <= n; i++) {
+      console.log(i);
+    }
+  }
 
 
-//   function printEvenNumbers(num) {
-//     for (let num = 1; num <= 100; num++) {
-//         if (num % 2 === 0) {
-//         console.log(num)
-//     }
-//   }
-// }
+  function printEvenNumbers(num) {
+    for (let num = 1; num <= 100; num++) {
+        if (num % 2 === 0) {
+        console.log(num)
+    }
+  }
+}
 
-//   printEvenNumbers(10)
+  printEvenNumbers(10)
 
 // Exercise 3: Calculate Sum
 // Write a function called calculateSum that takes a number 
@@ -60,39 +60,39 @@
 // Return the sum.
 // calcSum(10)
 
-// function calcSum(num) {
-//     for (let num = 1; num <= 20; num++) {
-//    console.log(calcSum);
-//     }
-// }
+function calcSum(num) {
+    for (let num = 1; num <= 20; num++) {
+   console.log(calcSum);
+    }
+}
 
 
-// let count = 1
+let count = 1
 
-// while (count <= 100) {
-// console.log(count)
-// count = count + 1
-// }
+while (count <= 100) {
+console.log(count)
+count = count + 1
+}
 
-// for (let i = 1; i <= 20; i++){
-//    if (i % 3 === 0 && i % 5 === 0) {
-//         console.log(`${i} --> FrontendSimplified`)
-//     }
-//     else if (i % 3 === 0) {
-//         console.log(`${i} --> frontend`)
-//     }
-//     else if (i % 5 === 0) {
-//         console.log(`${i} --> simplified`)
-//     }
+for (let i = 1; i <= 20; i++){
+   if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} --> FrontendSimplified`)
+    }
+    else if (i % 3 === 0) {
+        console.log(`${i} --> frontend`)
+    }
+    else if (i % 5 === 0) {
+        console.log(`${i} --> simplified`)
+    }
     
-//     else (console.log(`${i} --> ${i}`))
-// }
+    else (console.log(`${i} --> ${i}`))
+}
 
-// let str = "Frontend Simplified"
+let str = "Frontend Simplified"
 
-// for (i = 0; i <= str.length; i++) {
-//     console.log(str[i])
-// }
+for (i = 0; i <= str.length; i++) {
+    console.log(str[i])
+}
 
 function welcomeNameToFes(name, age) {
     console.log(`Welcome ${name} ${25} to FES`)
@@ -100,3 +100,16 @@ function welcomeNameToFes(name, age) {
 }
 
 welcomeNameToFes('David', '25');
+
+
+function fn() {
+    return 5
+    console.log('my function')
+}
+
+console.log(5);
+
+function sumOfTwoNumbers () {
+    return
+}
+
